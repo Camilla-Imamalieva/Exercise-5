5b18b51bb7386e432196eade32ad92f8922acdb3
Camillar <camillacrew@mail.ru>
2023-03-04 18:38:55 +0400

    Добавили задачу Exercise 3

---
+++ Exercise_3/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Наш массив состоит из чисел: ");
+PrintArray(numbers);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int z = 0; z < numbers.Length; z++)
+{
+    if (numbers[z] > max)
+    {
+        max = numbers[z];
+    }
+    if (numbers[z] < min)
+    {
+        min = numbers[z];
+    }
+}
+Console.WriteLine($"Всего {numbers.Length} чисел, где максимальное = {max}, минимальное = {min}");
+Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
+
+void FillArrayRandomNumbers(double[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = Convert.ToDouble(new Random().Next(100,1000));
+    }
+}
+void PrintArray(double[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    
+    Console.WriteLine();
+}
+
