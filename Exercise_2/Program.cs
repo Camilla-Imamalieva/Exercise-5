d216e5151a8e4347a4acebb05988dfc8bdc99cda
Camillar <camillacrew@mail.ru>
2023-03-04 18:21:07 +0400

    Добавили задачу Exercise 2

---
+++ Exercise_2/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Наш массив состоит из чисел: ");
+PrintArray(numbers);
+int sum = 0;
+
+for (int z = 0; z < numbers.Length; z+=2)
+    sum = sum + numbers[z];
+
+    Console.WriteLine($"Всего {numbers.Length} чисел, сумма элеметов, стоящих на нечетных позициях = {sum}");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(1,10);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    
+    Console.WriteLine();
+}
+
